30d9f191243ef886e78e1098257b0e138b49920b
ofirbakria <ofirbakria@gmail.com>
2024-07-09 18:16:31 +0300

    checking all piplines using trigger

--- final/piplines/argocd.groovy
+++ final/piplines/argocd.groovy
@@ -10,56 +10,17 @@ JOB.trigg_by = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').use
 PROP = [:]
 
 PROP['git_cred'] = 'github_ssh2'
-PROP['branch'] = 'oferbakria'
-PROP['proj_url'] = 'https://github.com/AlexeyMihaylovDev/atech-devops-nov-2023'
-
-PROP['dockerhub_cred'] = 'dockerhub_cred' // Jenkins credential ID for DockerHub
-PROP['docker_image1'] = 'oferbakria/poly'
-PROP['docker_image2'] = 'oferbakria/yolo'
-PROP['docker_tag'] = '1.7'
-PROP['aws_cli_cred'] = 'aws_cred'
-
+PROP['branch'] = 'main'
+PROP['proj_url'] = 'https://github.com/ofirbakria/final_project'
 PROP['project_folder_name'] = 'final'
-PROP['cluster_region'] = 'us-east-1'
-PROP['cluster_name'] = 'Atech'
-PROP['helm_sh'] = 'helm install ofertest -f values.yaml -n oferbakria .'
 
 
 pipeline {
 
     agent { label 'jenkins_ec2' } // Use the label of your EC2 agent
 
-    // triggers {
-    //     GenericTrigger(
-    //             genericVariables: [
-    //                     [key: 'refsb', value: '$.ref'],
-    //                     [key: 'pusher', value: '$.pusher.name'],
-    //                     [key: 'change_files', value: '$.commits[0].modified[0]'],
-    //                     // [key: 'type', value: '$.changes[0].type'],
-    //             ],
-    //             token: "123456",
-    //             tokenCredentialId: '',
-    //             printContributedVariables: true,
-    //             printPostContent: false,
-    //             silentResponse: false,
-    //             regexpFilterText: '$refsb $change_files',
-    //             regexpFilterExpression: '^(refs/heads/main)'        
-    //     )
-    // }
-
 
     stages {
-
-    // stage('Hello'){
-    //     steps {
-    //         script {
-    //             println("=====================================${STAGE_NAME}=====================================")
-    //             println("Hello ${PROP.trigg_by}")
-    //         }
-    //     }
-    // }
-
-
 
 
     stage('Git clone') {
--- final/piplines/pipline.groovy
+++ final/piplines/pipline.groovy
@@ -10,19 +10,19 @@ JOB.trigg_by = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').use
 PROP = [:]
 
 PROP['git_cred'] = 'github_ssh2'
-PROP['branch'] = 'oferbakria'
-PROP['proj_url'] = 'https://github.com/AlexeyMihaylovDev/atech-devops-nov-2023'
+PROP['branch'] = 'main'
+PROP['proj_url'] = 'https://github.com/ofirbakria/final_project'
 
 PROP['dockerhub_cred'] = 'dockerhub_cred' // Jenkins credential ID for DockerHub
 PROP['docker_image1'] = 'oferbakria/poly'
 PROP['docker_image2'] = 'oferbakria/yolo'
-PROP['docker_tag'] = '1.8'
+PROP['docker_tag'] = '1.9'
 PROP['aws_cli_cred'] = 'aws_cred'
 
 PROP['project_folder_name'] = 'final'
 PROP['cluster_region'] = 'us-east-1'
 PROP['cluster_name'] = 'Atech'
-PROP['helm_sh'] = 'helm install ofertest -f values.yaml -n oferbakria .'
+// PROP['helm_sh'] = 'helm install ofertest -f values.yaml -n oferbakria .'
 
 
 pipeline {
@@ -50,158 +50,169 @@ pipeline {
 
     stages {
 
-    stage('Hello'){
+    // stage('Hello'){
+    //     steps {
+    //         script {
+    //             println("=====================================${STAGE_NAME}=====================================")
+    //             println("Hello ${PROP.trigg_by}")
+    //         }
+    //     }
+    // }
+
+
+
+
+    stage('Git clone') {
         steps {
             script {
                 println("=====================================${STAGE_NAME}=====================================")
-                println("Hello ${PROP.trigg_by}")
+                println("Cloning from branch ${PROP.branch} and using credentials ${PROP.git_cred}")
+
+                // Enable sparse checkout
+                checkout([
+                    $class: 'GitSCM', 
+                    branches: [[name: "*/${PROP.branch}"]],
+                    doGenerateSubmoduleConfigurations: false, 
+                    extensions: [
+                        [$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: PROP.project_folder_name]]]
+                    ], 
+                    submoduleCfg: [], 
+                    userRemoteConfigs: [[
+                        credentialsId: PROP.git_cred, 
+                        url: PROP.proj_url
+                    ]]
+                ])
+            }
+        }
+        }
+
+
+        stage('Copy PEM File to Node') {
+            steps {
+                script {
+                    // Use the withCredentials block to securely retrieve the file credential
+                    withCredentials([file(credentialsId: 'alb2_cert', variable: 'CREDENTIAL_FILE')]) {
+                        def filePath = env.CREDENTIAL_FILE
+                        def destinationDir = '/home/ubuntu/workspace/final/final/aws_project/polybot'  // Replace with your actual destination directory
+
+                        // Copy the file to the node using Jenkins pipeline sh step
+                        sh "cp ${filePath} ${destinationDir}"
+                        sh "sudo chmod 666 ${destinationDir}/YOURPUBLIC.pem"
+                    }
+                }
+            }
+        }
+
+
+    stage('Create Docker Image') {
+        steps {
+             script {
+             println("=====================================${STAGE_NAME}=====================================")
+                sh "docker build -t ${PROP.docker_image1}:${PROP.docker_tag} ./final/aws_project/polybot"
+                sh "docker build -t ${PROP.docker_image2}:${PROP.docker_tag} ./final/aws_project/yolo5"
+             }
+        }
+    }
+
+    stage('Push Docker Images') {
+        steps {
+            script {
+                println("=====================================${STAGE_NAME}=====================================")
+                withCredentials([usernamePassword(credentialsId: PROP.dockerhub_cred, passwordVariable: 'DOCKERHUB_PASS', usernameVariable: 'DOCKERHUB_USER')]) {
+
+                    sh """
+                        echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_USER --password-stdin
+                        docker push ${PROP.docker_image1}:${PROP.docker_tag}
+                        docker push ${PROP.docker_image2}:${PROP.docker_tag}
+
+                        docker rmi -f ${PROP.docker_image1}:${PROP.docker_tag}
+                        docker rmi -f ${PROP.docker_image2}:${PROP.docker_tag}
+                    """
+                }
             }
         }
     }
 
 
 
-
-    // stage('Git clone') {
-    //     steps {
-    //         script {
-    //             println("=====================================${STAGE_NAME}=====================================")
-    //             println("Cloning from branch ${PROP.branch} and using credentials ${PROP.git_cred}")
-
-    //             // Enable sparse checkout
-    //             checkout([
-    //                 $class: 'GitSCM', 
-    //                 branches: [[name: "*/${PROP.branch}"]],
-    //                 doGenerateSubmoduleConfigurations: false, 
-    //                 extensions: [
-    //                     [$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: PROP.project_folder_name]]]
-    //                 ], 
-    //                 submoduleCfg: [], 
-    //                 userRemoteConfigs: [[
-    //                     credentialsId: PROP.git_cred, 
-    //                     url: PROP.proj_url
-    //                 ]]
-    //             ])
-    //         }
-    //     }
-    //     }
-
-
-    //     stage('Copy PEM File to Node') {
-    //         steps {
-    //             script {
-    //                 // Use the withCredentials block to securely retrieve the file credential
-    //                 withCredentials([file(credentialsId: 'alb2_cert', variable: 'CREDENTIAL_FILE')]) {
-    //                     def filePath = env.CREDENTIAL_FILE
-    //                     def destinationDir = '/home/ubuntu/workspace/final/final/aws_project/polybot'  // Replace with your actual destination directory
-
-    //                     // Copy the file to the node using Jenkins pipeline sh step
-    //                     sh "cp ${filePath} ${destinationDir}"
-    //                     sh "sudo chmod 666 ${destinationDir}/YOURPUBLIC.pem"
-    //                 }
-    //             }
-    //         }
-    //     }
-
-
-    // stage('Create Docker Image') {
-    //     steps {
-    //          script {
-    //          println("=====================================${STAGE_NAME}=====================================")
-    //             sh "docker build -t ${PROP.docker_image1}:${PROP.docker_tag} ./final/aws_project/polybot"
-    //             sh "docker build -t ${PROP.docker_image2}:${PROP.docker_tag} ./final/aws_project/yolo5"
-    //          }
-    //     }
-    // }
-
-    // stage('Push Docker Images') {
-    //     steps {
-    //         script {
-    //             println("=====================================${STAGE_NAME}=====================================")
-    //             withCredentials([usernamePassword(credentialsId: PROP.dockerhub_cred, passwordVariable: 'DOCKERHUB_PASS', usernameVariable: 'DOCKERHUB_USER')]) {
-
-    //                 sh """
-    //                     echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_USER --password-stdin
-    //                     docker push ${PROP.docker_image1}:${PROP.docker_tag}
-    //                     docker push ${PROP.docker_image2}:${PROP.docker_tag}
-
-    //                     docker rmi -f ${PROP.docker_image1}:${PROP.docker_tag}
-    //                     docker rmi -f ${PROP.docker_image2}:${PROP.docker_tag}
-    //                 """
-    //             }
-    //         }
-    //     }
-    // }
-
-
-
-    // stage('Read helm chart values'){
-    //     steps{
-    //         script{  
-    //                 dir("${WORKSPACE}/${PROP.project_folder_name}")  {
-    //                 sh 'ls -l'    
-    //             def chartValues = readFile (file: 'values.yaml')  
-    //             JOB.buildChartValues = chartValues
-    //                 }
-    //             println(JOB.buildChartValues)
+    stage('Read helm chart values'){
+        steps{
+            script{  
+                    dir("${WORKSPACE}/${PROP.project_folder_name}")  {
+                    sh 'ls -l'    
+                def chartValues = readFile (file: 'values.yaml')  
+                JOB.buildChartValues = chartValues
+                    }
+                println(JOB.buildChartValues)
                             
-    //         }
+            }
             
-    //     }
-    // }
+        }
+    }
 
 
 
-    // stage('Input params '){
-    //     when{
-    //         expression { !JOB.trigg_by.isEmpty()}
-    //     }
-    //     steps{
-    //         script{
-    //             def userInput = input (id: 'userInput', message: 'Please provide the following parameters', parameters: [
-    //                 [$class: 'WHideParameterDefinition', defaultValue: JOB.buildChartValues, description: '', name: 'chartValues'],
-    //                 choice(name: 'CHOICE_HELM', choices: ['install','upgrade','uninstall'], description: 'Choose one'),
-    //                 [$class: 'DynamicReferenceParameter', choiceType: 'ET_FORMATTED_HTML', description: "",
-    //                         name: 'ChartValues', omitValueField: true, referencedParameters: 'chartValues',
-    //                         script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true,
-    //                                                                         script: 'return [\'error\']'], script: [classpath: [], sandbox: true, script: '''
-    //                         def jsonValue = "${chartValues}".replaceAll('"', '\\\\"') 
-    //                         return "<textarea name=\\"value\\" rows='27' cols='100'>${jsonValue}</textarea>"
-    //                     ''']]],
+    stage('Input params '){
+        when{
+            expression { !JOB.trigg_by.isEmpty()}
+        }
+        steps{
+            script{
+                def userInput = input (id: 'userInput', message: 'Please provide the following parameters', parameters: [
+                    [$class: 'WHideParameterDefinition', defaultValue: JOB.buildChartValues, description: '', name: 'chartValues'],
+                    choice(name: 'CHOICE_HELM', choices: ['install','upgrade','uninstall'], description: 'Choose one'),
+                    [$class: 'DynamicReferenceParameter', choiceType: 'ET_FORMATTED_HTML', description: "",
+                            name: 'ChartValues', omitValueField: true, referencedParameters: 'chartValues',
+                            script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true,
+                                                                            script: 'return [\'error\']'], script: [classpath: [], sandbox: true, script: '''
+                            def jsonValue = "${chartValues}".replaceAll('"', '\\\\"') 
+                            return "<textarea name=\\"value\\" rows='27' cols='100'>${jsonValue}</textarea>"
+                        ''']]],
 
-    //             ])
-    //             JOB.params = userInput['ChartValues']
-    //             sh 'rm -f values.yaml' //delete old values.yaml
+                ])
+                JOB.params = userInput['ChartValues']
+                sh 'rm -f values.yaml' //delete old values.yaml
 
-    //         }
-    //     }
-    // }
-
-
-
-    //     stage('Helm update values by user input'){
-    //         when{
-    //             expression { !JOB.trigg_by.isEmpty()}
-    //         }
-    //         steps{
-    //             script{
-    //                  dir("${WORKSPACE}/${PROP.project_folder_name}")  {
-    //                     writeFile file: 'values.yaml', text: JOB.params
-    //                 }
-    //             }
-    //         }
-    //     }
+            }
+        }
+    }
 
 
-    //     stage('Login to EKS') {
-    //         steps {
-    //             withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: PROP['aws_cli_cred']]]) {
-    //                 script {
-    //                     sh "aws eks --region ${PROP.cluster_region} update-kubeconfig --name ${PROP.cluster_name}"
-    //                 }
-    //             }
-    //         }
-    //     }
+
+        stage('Helm update values by user input'){
+            when{
+                expression { !JOB.trigg_by.isEmpty()}
+            }
+            steps{
+                script{
+                     dir("${WORKSPACE}/${PROP.project_folder_name}")  {
+                        writeFile file: 'values.yaml', text: JOB.params
+                    }
+                }
+            }
+        }
+
+
+        stage('Login to EKS') {
+            steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: PROP['aws_cli_cred']]]) {
+                    script {
+                        sh "aws eks --region ${PROP.cluster_region} update-kubeconfig --name ${PROP.cluster_name}"
+                    }
+                }
+            }
+        }
+
+
+
+
+        stage('Trigger Argocd') {
+            steps {
+                build job: 'argocd', wait: true
+            }
+        }
+
+
 
 
 
@@ -214,8 +225,6 @@ pipeline {
     //             }
     //         }
     //     }
-
-
 
 
 
